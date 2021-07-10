0e22c84500cf2ba2ad91ef87a744410b9220413f
Gene Kogan <kogan.gene@gmail.com>
2021-07-09 23:37:43 -0400

    changes to generator, offloading saving static images

--- core/augmentation.py
+++ core/augmentation.py
@@ -2,6 +2,6 @@ import torch
 import torchvision
 
 Augmentations = torch.nn.Sequential(
-        torchvision.transforms.RandomHorizontalFlip(),
-        torchvision.transforms.RandomAffine(24, (.1, .1))#, fill=0)
-    )
+    torchvision.transforms.RandomHorizontalFlip(),
+    torchvision.transforms.RandomAffine(24, (.1, .1))#, fill=0)
+)
--- eden_server.py
+++ eden_server.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import json
+import copy
 import torch 
 import clip
 import PIL
@@ -11,11 +12,8 @@ from eden.block import BaseBlock
 from eden.datatypes import Image
 from eden.hosting import host_block
 
-
-
-RESULTS_DIR =  '/home/bzion/ml4a/abraham/static/results'  ## '/home/bzion/ml4a/abraham/static/results'
-
 eden_block = BaseBlock()
+eden_block.max_gpu_mem = 1.0
 
 def get_models(config):
 
@@ -27,7 +25,7 @@ def get_models(config):
     print("SETUP CLIP ON cuda:{}".format(gpu_idx))
 
     taming_transformers.gpu = gpu_idx
-    taming_transformers.setup('vqgan')
+    taming_transformers.setup('imagenet')
     print("SETUP TAMING ON cuda:{}".format(gpu_idx))
     print(f'setup complete, transformer on: {id(taming_transformers)}')
 
@@ -37,12 +35,14 @@ my_args = {
     'prompt': 'hello world',
     'width': 256,
     'height': 256,
-    'iters': 300,
+    'num_octaves': 3,
+    'octave_scale': 2.0,
+    'num_iterations': 10,
     'weight_decay': 0.1,
     'learning_rate': 0.1,
     'lr_decay_after': 400,
     'lr_decay_rate': 0.995
-}
+}    
 
 @eden_block.run(
     args = my_args
@@ -51,7 +51,6 @@ def run(config):
 
     print("the config \n ")
     print(config)
-
     print(f"gpu for {config['username']}  is ", config['__gpu__'])
 
     taming_transformers, perceptor, preprocess = get_models(config = config)
@@ -60,38 +59,41 @@ def run(config):
     model.post_quant_conv = model.post_quant_conv.to(config['__gpu__'])
     model.decoder = model.decoder.to(config['__gpu__'])
 
+    # from copy import deepcopy
+
+    # taming_transformers.model = deepcopy(taming_transformers.model)
+
+    config['num_iterations'] = [100, 300, 300]
+    config['text_inputs'] = [{
+        'text': config['prompt'],
+        'weight': 10.0
+    }]
+    
+
     try:
-        img = generate(config, perceptor =  perceptor, preprocess = preprocess, 
-                        model = model, device = config['__gpu__'])
+
+        width, height = config['width'], config['height']
+        octave_scale, num_octaves = config['octave_scale'], config['num_octaves']
+
+        img = None
+
+        for octave in range(3):
+            config_octave = config.copy()
+
+            config_octave['width'] = int(width * (octave_scale ** -(num_octaves-octave-1)))
+            config_octave['height'] = int(height * (octave_scale ** -(num_octaves-octave-1)))
+            config_octave['num_iterations'] = config['num_iterations'][octave]
+            
+            img = generate(config_octave, perceptor = perceptor, preprocess = preprocess, model = model, device = config['__gpu__'], img = img)
 
     except Exception as e:
 
         print('prompt', config['prompt'])
         raise Exception(str(e))
 
-        
-    print(sorted(glob.glob(f'{RESULTS_DIR}/*')), ' sorted thingy')
-    try:
-        last_idx = int(sorted(glob.glob(f'{RESULTS_DIR}/*'))[-1].split('/')[-1])
-    except:
-        last_idx = 0
-    idx = 1 + last_idx
-    output_dir = f'{RESULTS_DIR}/%04d'%idx
-    print('last idx', last_idx)
-    image_path = '{}/{}'.format(output_dir, 'image.jpg')
-    config_path = '{}/{}'.format(output_dir, 'config.json')        
-    
-    if not os.path.isdir(output_dir):
-        os.mkdir(output_dir)
-
-    with open(config_path, 'w') as outfile:
-        json.dump(config, outfile)
-
-    PIL.Image.fromarray(img).save(image_path)
-    
-
     return {
-        'creation': Image(img)
+        'creation': Image(img),
+        'config': config
     }
 
 host_block(
