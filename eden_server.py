31684fd7d14aa9da8434ce3e54526ab54e515c22
Gene Kogan <kogan.gene@gmail.com>
2021-07-16 12:47:16 -0400

    overhaul taming-transformers module

--- eden_server.py
+++ eden_server.py
@@ -3,9 +3,12 @@ import glob
 import json
 import copy
 import torch 
-import clip
 import PIL
-from dotenv import load_dotenv
+from omegaconf import OmegaConf
+
+import clip
+import taming.modules.losses
+from taming.models.vqgan import VQModel
 
 from core.generate import generate
 
@@ -13,29 +16,10 @@ from eden.block import BaseBlock
 from eden.datatypes import Image
 from eden.hosting import host_block
 
-eden_block = BaseBlock()
-eden_block.max_gpu_mem = 1.0
+eden_block = BaseBlock(max_gpu_mem = 1)
 
-
-pretrained_vqgan = {
-    'imagenet': {
-        'checkpoint': 'https://heibox.uni-heidelberg.de/f/867b05fc8c4841768640/?dl=1',
-        'config': 'https://heibox.uni-heidelberg.de/f/274fb24ed38341bfa753/?dl=1'
-    },
-    'wikiart': {
-        'checkpoint': 'http://mirror.io.community/blob/vqgan/wikiart_16384.ckpt',
-        'config': 'http://mirror.io.community/blob/vqgan/wikiart_16384.yaml'
-    },
-}
 
 def get_models(config):
-
-    import taming.modules.losses
-    from taming.models.vqgan import VQModel
-    from omegaconf import OmegaConf
-    from ml4a.utils import downloads
-
-    print("the config is ")
     print(config)
 
     # load CLIP
@@ -43,16 +27,15 @@ def get_models(config):
     perceptor = perceptor.eval()
 
     # load VQGAN
-    model_name = 'imagenet'
-    
-    checkpoint = downloads.download_data_file(
-        pretrained_vqgan[model_name]['checkpoint'],
-        'taming-transformers/{}/checkpoint.ckpt'.format(model_name))
-
-    config_file = downloads.download_data_file(
-        pretrained_vqgan[model_name]['config'],
-        'taming-transformers/{}/config.yaml'.format(model_name))
-
+    model_name = config['model_name']
+    assert model_name in ['imagenet', 'wikiart'], \
+        'No model named {}. Available models are: imagenet, wikiart'.format(model_name)
+    if model_name == 'imagenet':
+        checkpoint = 'pretrained/imagenet/last.ckpt'
+        config_file = 'pretrained/imagenet/model.yaml'
+    elif model_name == 'wikiart':
+        checkpoint = 'pretrained/wikiart/wikiart_16384.ckpt'
+        config_file = 'pretrained/wikiart/wikiart_16384.yaml'
     taming_config = OmegaConf.load(config_file)
     model = VQModel(**taming_config.model.params)
     sd = torch.load(checkpoint, map_location="cpu")["state_dict"]
@@ -62,13 +45,14 @@ def get_models(config):
     model = model.to(config['__gpu__'])
     #torch.set_grad_enabled(False)
 
-    print("SETUP CLIP ON {}".format(config['__gpu__']))
-    print("SETUP TAMING ON {}".format(config['__gpu__']))
+    print("Setup CLIP on {}".format(config['__gpu__']))
+    print("Setup VQGAN on {}".format(config['__gpu__']))
 
     return model, perceptor, preprocess
 
 
 my_args = {
+    'model_name': 'imagenet',
     'text_inputs': [{
         'text': 'hello world',
         'weight': 10.0
@@ -77,7 +61,7 @@ my_args = {
     'height': 256,
     'num_octaves': 3,
     'octave_scale': 2.0,
-    'num_iterations': [10, 20, 30],
+    'num_iterations': [100, 200, 300],
     'weight_decay': 0.1,
     'learning_rate': 0.1,
     'lr_decay_after': 400,
@@ -88,10 +72,11 @@ my_args = {
     progress = True
 )
 def run(config):
+    print('STARTED RUN')
 
     print("config: \n")
     print(config)
-    print(f"gpu for {config['username']}  is ", config['__gpu__'])
+    #print(f"gpu for {config['username']}  is ", config['__gpu__'])
 
     model, perceptor, preprocess = get_models(config = config)
 
@@ -99,19 +84,22 @@ def run(config):
     model.post_quant_conv = model.post_quant_conv.to(config['__gpu__'])
     model.decoder = model.decoder.to(config['__gpu__'])
 
-    print(config)
-    
     try:
         width, height = config['width'], config['height']
         octave_scale, num_octaves = config['octave_scale'], config['num_octaves']
+        progress = config['__progress__']
+
+        '''
+        This block uses a heuristic to estimate the progress_step_size at each octave
+        '''
+        #total_progress = [n*(config['octave_scale']**(2*i)) for i, n in enumerate(config['num_iterations'])]
+        iter_per_sec = [4.962, 3.826, 1.964]  # determined empirically for sizes 128, 256, 512
+        total_progress = [n/i for i, n in zip(iter_per_sec, config['num_iterations'])]
+        progress_step_sizes = [t/(n * sum(total_progress)) for n, t in zip(config['num_iterations'], total_progress)]
 
         img = None
 
-        num_total_steps = sum(config['num_iterations'])
-        progress = config['__progress__']
-        progress_step_size = 1/num_total_steps
-
-        for octave in range(3):
+        for octave in range(config['num_octaves']):
             config_octave = config.copy()
 
             config_octave['width'] = int(width * (octave_scale ** -(num_octaves-octave-1)))
@@ -119,37 +107,22 @@ def run(config):
             config_octave['num_iterations'] = config['num_iterations'][octave]
             config_octave['lr_decay_after'] = int(config_octave['num_iterations'] * 0.5)
 
+            progress_step_size = progress_step_sizes[octave]
             img = generate(config_octave, perceptor = perceptor, preprocess = preprocess, model = model, device = config['__gpu__'], img = img, progress = progress, progress_step_size = progress_step_size)
-
-        # load_dotenv()
-        # RESULTS_DIR = os.environ['RESULTS_DIR']  
-        # try:
-        #     last_idx = int(sorted(glob.glob(f'{RESULTS_DIR}/*'))[-1].split('/')[-1])
-        # except:
-        #     last_idx = 0
-        # idx = 1 + last_idx
-        # output_dir = f'{RESULTS_DIR}/%04d'%idx
-        # image_path = '{}/{}'.format(output_dir, 'image.jpg')
-        # config_path = '{}/{}'.format(output_dir, 'config.json')        
-
-        # if not os.path.isdir(output_dir):
-        #     os.mkdir(output_dir)
-
-        # with open(config_path, 'w') as outfile:
-        #     json.dump(config, outfile)
-
-        # PIL.Image.fromarray(img).save(image_path)
-
 
     except Exception as e:
         raise Exception(str(e))
 
+    config.pop('__progress__')
+    
     return {
         'creation': Image(img),
+        'config': config
     }
 
 host_block(
     eden_block,
     port = 5454,
-    max_num_workers = 2
+    max_num_workers = 4,
+    redis_port = 6381
 )
