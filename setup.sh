f27580034555d2faf3416d8eabd4be3e64e3f3c6
Daniel Radu <radu.daniel@gmail.com>
2021-10-02 21:51:16 +0200

    fix: missing dependencies

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-pip install git+https://github.com/abraham-ai/eden.git --no-deps
+pip install git+https://github.com/abraham-ai/eden.git
 pip install git+https://github.com/openai/CLIP.git --no-deps
 sh download_models.sh
 pip install -r requirements.txt
