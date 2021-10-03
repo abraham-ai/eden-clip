d1ac2594ba97c6b0ff9b960dfe5e21eef0735b44
mayukhdeb <mayukhmainak2000@gmail.com>
2021-10-03 12:43:18 +0530

    fix broken clip install on docker build

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
+sudo apt install python3.8-venv
+python3 -m venv env-eden-clip
+source env-eden-clip/bin/activate
 pip install --upgrade pip
+pip install https://github.com/openai/CLIP/archive/refs/heads/main>
 pip install git+https://github.com/abraham-ai/eden.git
-pip install git+https://github.com/openai/CLIP.git --no-deps
 sh download_models.sh
 pip install -r requirements.txt
 echo "Setup complete!"
