666596783b66b684989128c817d6048b690bc2e1
Daniel Radu <radu.daniel@gmail.com>
2021-11-02 21:46:34 +0100

    chore: fix conflict

--- eden_server.py
+++ eden_server.py
@@ -164,9 +164,5 @@ host_block(
     redis_port = args.redis_port,
     redis_host = args.redis_host,
     logfile = args.logfile,  ## let us dump logs to stdout
-<<<<<<< HEAD
-    loglevel = 'debug'
-=======
     loglevel = 'debug' 
->>>>>>> f6b2706d55cbbd26b4e6afef52260ef6132d9ebd
 )
