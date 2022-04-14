1d064a6dbeedbefe9a7fb941b5f49c41fe59001a
Daniel Radu <radu.daniel@gmail.com>
2022-04-14 17:32:11 +0200

    chore: hacking

--- download_models.sh
+++ download_models.sh
@@ -1,6 +1,6 @@
 mkdir -p pretrained/imagenet
-wget -O pretrained/imagenet/last.ckpt https://heibox.uni-heidelberg.de/f/867b05fc8c4841768640/?dl=1
-wget -O pretrained/imagenet/model.yaml https://heibox.uni-heidelberg.de/f/274fb24ed38341bfa753/?dl=1
+wget -nv -O pretrained/imagenet/last.ckpt https://heibox.uni-heidelberg.de/f/867b05fc8c4841768640/?dl=1
+wget -nv -O pretrained/imagenet/model.yaml https://heibox.uni-heidelberg.de/f/274fb24ed38341bfa753/?dl=1
 # mkdir -p pretrained/wikiart
 # wget -O pretrained/wikiart/wikiart_16384.ckpt http://mirror.io.community/blob/vqgan/wikiart_16384.ckpt
 # wget -O pretrained/wikiart/wikiart_16384.yaml http://mirror.io.community/blob/vqgan/wikiart_16384.yaml
--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ pip install taming-transformers==0.0.1 --no-deps
 pip install git+https://github.com/abraham-ai/eden.git
 sh download_models.sh
 pip install -r requirements.txt
+pip3 install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html # did not work in requirements.txt
 echo "Setup complete!"
